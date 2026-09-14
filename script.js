972926cc65ab22f3d27b2dc26fb0bb0ee038b2e7
anagronn <nastasya.vodchits@mail.ru>
2026-09-15 00:34:04 +0300

    feat: add download functionality, favicon, some styles

--- script.js
+++ script.js
@@ -14,3 +14,41 @@ darkMode.addEventListener('click', () => {
   darkMode.classList.add('active');
   lightMode.classList.remove('active');
 });
+
+const downloadButton = document.getElementById('download-pdf');
+const cv = document.querySelector('.main-flex-container');
+
+downloadButton.addEventListener('click', () => {
+  let filename = 'Anastasia_Vodchits_CV_EN.pdf';
+
+  if (document.documentElement.lang === 'es') {
+    filename = 'Anastasia_Vodchits_CV_ES.pdf';
+  }
+
+  if (document.documentElement.lang === 'ru') {
+    filename = 'Anastasia_Vodchits_CV_RU.pdf';
+  }
+
+  const options = {
+    margin: 0,
+    filename: filename,
+    image: {
+      type: 'jpeg',
+      quality: 0.98
+    },
+    html2canvas: {
+      scale: 2,
+      useCORS: true
+    },
+    jsPDF: {
+      unit: 'mm',
+      format: 'a4',
+      orientation: 'portrait'
+    }
+  };
+
+  html2pdf()
+    .set(options)
+    .from(cv)
+    .save();
+});
